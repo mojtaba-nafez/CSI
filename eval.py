293b68e5471dcbb8c0575520b9b947cb541b3284
mojtaba nafez <mojtabanafez96@gmail.com>
2023-10-28 00:25:54 +0330

    update

--- eval.py
+++ eval.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 import models.classifier as C
 from datasets import get_dataset, get_superclass_list, get_subclass_dataset
 from copy import deepcopy
-from evaluation.eval import evaluate
+from evals.ood_pre import eval_ood_detection as evaluate 
 from argparse import ArgumentParser
 
 
