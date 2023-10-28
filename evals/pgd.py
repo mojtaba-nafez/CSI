0bfbee45bd8d18bccaecce57bc348df732a04d62
mojtaba nafez <mojtabanafez96@gmail.com>
2023-10-28 17:10:17 +0330

    update

--- evals/pgd.py
+++ evals/pgd.py
@@ -27,6 +27,7 @@ class PGD(Attack):
     def __init__(self, model, eps=8 / 255,
                  alpha=2 / 255, steps=10, random_start=True):
         super().__init__("PGD", model)
+        print("eps, alpha: ", eps, alpha)
         self.eps = eps
         self.alpha = alpha
         self.steps = steps
