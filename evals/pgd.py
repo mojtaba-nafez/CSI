bb5414d70dc4658721bd706bf365f3d5b21e7e99
mojtaba nafez <mojtabanafez96@gmail.com>
2023-10-28 16:02:02 +0330

    update

--- evals/pgd.py
+++ evals/pgd.py
@@ -56,7 +56,7 @@ class PGD(Attack):
             if is_normal:
                 label = torch.zeros((outputs.shape[0],2))
                 label[:,1] = 1.0
-                cost = loss(outputs.to(self.device), label.type(torch.LongTensor).to(self.device))
+                cost = loss(outputs.to(self.device), label.type(torch.LongTensor).to(self.device)).to(device)
             else:
                 label = torch.zeros((outputs.shape[0],2))
                 label[:,0] = 1.0
