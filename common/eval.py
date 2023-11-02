43fb035b1e16a23271a2cc0765a3479f82b14d26
mojtaba nafez <mojtabanafez96@gmail.com>
2023-11-02 16:45:09 +0330

    update

--- common/eval.py
+++ common/eval.py
@@ -99,7 +99,7 @@ simclr_aug = C.get_simclr_augmentation(P, image_size=P.image_size).to(device)
 P.shift_trans, P.K_shift = C.get_shift_module(P, eval=True)
 P.shift_trans = P.shift_trans.to(device)
 
-P.K_shift = 2
+P.K_shift = 11
 model = C.get_classifier(P.model, n_classes=P.n_classes, activation=P.activation_function, mean=P.noise_mean, std=P.noise_std, noise_scale=P.noise_scale, noist_probability=P.noist_probability).to(device)
 model = C.get_shift_classifer(model, P.K_shift).to(device)
 criterion = nn.CrossEntropyLoss().to(device)
