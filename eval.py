e2724e8f4bcb950fd7bbaa0c3f82699ffb3ebfa1
mojtaba nafez <mojtabanafez96@gmail.com>
2024-06-25 04:25:17 +0330

    update

--- eval.py
+++ eval.py
@@ -3,42 +3,36 @@ from common.eval import *
 model.eval()
 print(P)
 
-elif P.mode in ['ood', 'ood_pre']:
-    if P.mode == 'ood':
-        from evals import eval_ood_detection
-    else:
-        from evals.ood_pre import eval_ood_detection
-
-    with torch.no_grad():
-        auroc_dict = eval_ood_detection(P, model, test_loader, ood_test_loader, P.ood_score,
-                                        train_loader=train_loader, simclr_aug=simclr_aug)
-
-    if P.one_class_idx is not None:
-        mean_dict = dict()
-        for ood_score in P.ood_score:
-            mean = 0
-            for ood in auroc_dict.keys():
-                mean += auroc_dict[ood][ood_score]
-            mean_dict[ood_score] = mean / len(auroc_dict.keys())
-        auroc_dict['one_class_mean'] = mean_dict
-
-    bests = []
-    for ood in auroc_dict.keys():
-        message = ''
-        best_auroc = 0
-        for ood_score, auroc in auroc_dict[ood].items():
-            message += '[%s %s %.4f] ' % (ood, ood_score, auroc)
-            if auroc > best_auroc:
-                best_auroc = auroc
-        message += '[%s %s %.4f] ' % (ood, 'best', best_auroc)
-        if P.print_score:
-            print(message)
-        bests.append(best_auroc)
-
-    bests = map('{:.4f}'.format, bests)
-    print('\t'.join(bests))    
-
+if P.mode == 'ood':
+    from evals import eval_ood_detection
 else:
-    raise NotImplementedError()
+    from evals.ood_pre import eval_ood_detection
 
+with torch.no_grad():
+    auroc_dict = eval_ood_detection(P, model, test_loader, ood_test_loader, P.ood_score,
+                                    train_loader=train_loader, simclr_aug=simclr_aug)
 
+if P.one_class_idx is not None:
+    mean_dict = dict()
+    for ood_score in P.ood_score:
+        mean = 0
+        for ood in auroc_dict.keys():
+            mean += auroc_dict[ood][ood_score]
+        mean_dict[ood_score] = mean / len(auroc_dict.keys())
+    auroc_dict['one_class_mean'] = mean_dict
+
+bests = []
+for ood in auroc_dict.keys():
+    message = ''
+    best_auroc = 0
+    for ood_score, auroc in auroc_dict[ood].items():
+        message += '[%s %s %.4f] ' % (ood, ood_score, auroc)
+        if auroc > best_auroc:
+            best_auroc = auroc
+    message += '[%s %s %.4f] ' % (ood, 'best', best_auroc)
+    if P.print_score:
+        print(message)
+    bests.append(best_auroc)
+
+bests = map('{:.4f}'.format, bests)
+print('\t'.join(bests))
