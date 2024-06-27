0e2e9fa3db58edd8116344c0060d3bdb031d645d
mojtaba nafez <mojtabanafez96@gmail.com>
2024-06-27 23:22:35 +0330

    update

--- models/classifier.py
+++ models/classifier.py
@@ -44,7 +44,7 @@ def get_classifier(mode, n_classes=10, activation='relu', freezing_layer=133):
         classifier = Pretrain_Wide_ResNet_Model(num_classes=n_classes)
     elif mode == 'resnet18':
         classifier = ResNet18(num_classes=n_classes)
-    elif mode == 'ResNet':
+    elif mode == 'pretrain-resnet18':
         classifier = Pretrain_ResNet18_Model(num_classes=n_classes)
     elif mode == 'custome_resnet18':
         classifier = resnet18(num_classes=n_classes)
