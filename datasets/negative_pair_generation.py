99a51bf00c9fbb1b99ee7270fea8a5d76466705b
mojtaba nafez <mojtabanafez96@gmail.com>
2024-06-27 15:36:59 +0330

    update

--- datasets/negative_pair_generation.py
+++ datasets/negative_pair_generation.py
@@ -4,9 +4,45 @@ import torch.nn.functional as F
 import random
 import models.transform_layers as TL
 import json
+from torchvision import datasets, transforms
+from glob import glob
+from PIL import Image
 
+
+class ImageNetExposure(Dataset):
+    def __init__(self, root, count, transform=None):
+        self.transform = transform
+        image_files = glob(os.path.join(root, 'train', "*", "images", "*.JPEG"))
+        if count==-1:
+            final_length = len(image_files)
+        else:
+            random.shuffle(image_files)
+            final_length = min(len(image_files), count)
+        self.image_files = image_files[:final_length]
+        self.image_files.sort(key=lambda y: y.lower())
+
+    def __getitem__(self, index):
+        image_file = self.image_files[index]
+        image = Image.open(image_file)
+        image = image.convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, -1
+
+    def __len__(self):
+        return len(self.image_files)
+        
 class NegativePairGenerator:
     def __init__(self):
+        tiny_transform = transforms.Compose([
+                transforms.Resize((32, 32)),
+                transforms.AutoAugment(),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor()
+        ])
+        imagenet_exposure = ImageNetExposure(root='./tiny-imagenet-200', count=10000, transform=tiny_transform)
+        self.imagenet_exposure_loader = DataLoader(imagenet_exposure, shuffle=True, batch_size=1024)
+
         # self.probabilities = {'rotation': 0.0, 'cutperm': 0.0, 'cutout': 0.01, 'cutpaste': 0.99}
         with open('./config.json', 'r') as json_file:
             self.probabilities = json.load(json_file)
@@ -43,6 +79,9 @@ class NegativePairGenerator:
         return self.cutpaste_shift(img.unsqueeze(0)).squeeze()
 
     def create_negative_pair(self, batch_image):
+        x, _ = next(iter(self.imagenet_exposure_loader))
+        return x.to(self.device)
+        
         batch_image = batch_image.to(self.device)   
         augs = list(self.probabilities.keys())
         probs = list(self.probabilities.values())            
